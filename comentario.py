efd28ee77a6698dd60cd4badb6ff5f03dce77278
Sebastian Quispe <sebastian.quispe.b@utec.edu.pe>
2026-06-20 00:36:54 -0500

    Add S3 ingestion bucket and Push strategy to save comments

--- comentario.py
+++ comentario.py
@@ -1,6 +1,10 @@
 import boto3
 import uuid
 import os
+import json
+from datetime import datetime
+
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -8,6 +12,7 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    bucket_name = os.environ["BUCKET_NAME"]
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
@@ -20,10 +25,19 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+    # Guardar en S3 (Push ingestion)
+    filename = f"comentarios/{datetime.now().isoformat()}_{uuidv1}.json"
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=filename,
+        Body=json.dumps(comentario),
+        ContentType='application/json'
+    )
     # Salida (json)
     print(comentario)
     return {
         'statusCode': 200,
         'comentario': comentario,
-        'response': response
+        'response': response,
+        's3_file': filename
     }
